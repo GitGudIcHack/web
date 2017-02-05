dfe00431add36ebcf6ce29a5efce527cd30cd730
Boris Barath <borisbarath@dyn1204-157.wlan.ic.ac.uk>
2017-02-05 06:23:04 +0000

    ad

--- fun.js
+++ fun.js
@@ -31,3 +31,14 @@ document.querySelector('#searchbar').addEventListener('keypress', function (e) {
 $("mdl-button").on("click", function(event){
     $(this).attr("disabled", true);
 });
+
+function cloneThing(elem) {
+  var temp = $("#finalcard"),
+  cloneObj = temp.clone(true, true);
+  var clone = cloneObj.get(0)
+  clone.id = 'newid' + clone.id
+  clone.classList.remove("hidden");
+  searchstring.focus();
+  clone.getElementsByTagName("h4").cardname.textContent = elem.question_val;
+  $('#overview').prepend(cloneObj);
+}
